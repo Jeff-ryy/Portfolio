33075e1aad2039e28f9c6f09643322e6cb39dde8
Ubuntu <ubuntu@ip-172-31-8-169.ap-south-1.compute.internal>
2024-12-09 09:08:05 +0000

    adding frontend

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // Allows access from your IP
+    port: 3000,      // Replace with your desired port
+  },
   plugins: [react()],
-})
+});
+
